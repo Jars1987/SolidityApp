badd7978c6278edf03c350f5b78e2511fe75b1ec
José Santos <josesantos@MacBook-Pro-de-Jose.local>
2022-02-25 16:51:26 +0000

    Solidity 101

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {
+  InputBox,
+  InputBoxContainer,
+  InputBoxItems,
+  Input,
+} from './components/AppElements';
+import Web3 from 'web3';
+import { changeStateInput } from './abi/abis';
+
+//Truffle outputs post-migation process
+const web3 = new Web3(Web3.givenProvider);
+const contractAddr = '0xf2c4F25eaA79768f92F700217E1114DC1539762E';
+const ChangeState = new web3.eth.Contract(changeStateInput, contractAddr);
 
 function App() {
+  const [state, setState] = useState(0);
+  const [getState, setGetState] = useState('Click to refresh');
+
+  const web3check = new Web3();
+  web3check.setProvider(
+    new Web3.providers.WebsocketProvider('ws://localhost:9545')
+  );
+  web3check.eth.net
+    .isListening()
+    .then(() => console.log('Connection Successfuly'))
+    .catch(e => console.log('Something went wrong!', e));
+
+  const handleGet = async e => {
+    e.preventDefault();
+    const result = await ChangeState.methods.get().call();
+    setGetState(result);
+    console.log(result);
+  };
+
+  const handleSet = async e => {
+    e.preventDefault();
+    const accounts = await window.ethereum.enable();
+    const account = accounts[0];
+    const gas = await ChangeState.methods.set(state).estimateGas();
+    const result = await ChangeState.methods.set(state).send({
+      from: account,
+      gas,
+    });
+    console.log(result);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <InputBoxContainer>
+        <InputBox>
+          <InputBoxItems>
+            <h1>State Change Dapp</h1>
+            <form onSubmit={handleSet}>
+              <label htmlFor='state'>
+                My Input:
+                <Input
+                  id='state'
+                  type='number'
+                  name='stateInput'
+                  value={state}
+                  onChange={e => setState(e.target.value)}
+                />
+              </label>
+              <br />
+              <input type='submit' value='Set State' />
+            </form>
+            <br />
+            <button onClick={handleGet}>Get State</button>
+            <br />
+            <br />
+            <div> {getState}</div>
+          </InputBoxItems>
+        </InputBox>
+      </InputBoxContainer>
     </div>
   );
 }
---
+++ src/components/AppElements.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const InputBoxContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+export const InputBox = styled.div`
+  background-color: #5cb3ff;
+  color: white;
+  width: 60%;
+  height: 250px;
+  position: relative;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 30px;
+`;
+
+export const InputBoxItems = styled.div`
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  position: relative;
+`;
+
+export const Input = styled.input`
+  height: 40px;
+  font-size: 20pt;
+  width: 150px;
+  margin-bottom: ;
+`;
